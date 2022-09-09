b1bb2e9167ff5c7408f023d75d6ec935f00cfab4
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-09-08 23:59:30 -0400

    fix login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -30,12 +30,6 @@ const LoginForm = () => {
 
     try {
       const { data } = await loginUser({ variables: { ...userFormData } });
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
       Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
