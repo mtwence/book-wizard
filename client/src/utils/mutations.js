b727f496b88c9d115d631fe4948b6da395c9ea53
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-09-08 21:47:14 -0400

    add client side mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,97 @@
+import { gql } from "@apollo/client";
+
+// Mutations
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          _id
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
+
+// Queries 
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
