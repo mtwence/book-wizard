36efa4b360802dbc8feab988c692a387fed792e3
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-09-08 23:57:07 -0400

    import useMutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { ADD_USER } from "../utils/mutations";
-import { createUser } from '../utils/API';
+import { useMutation } from "@apollo/client";
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
