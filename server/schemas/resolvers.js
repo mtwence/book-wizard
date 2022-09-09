d0b75d03bf1f54c9810cf7565edf9010d4273812
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-09-08 21:12:44 -0400

    add login and aduser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,4 +12,29 @@ const resolvers = {
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-  }
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Password");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+},
+};
